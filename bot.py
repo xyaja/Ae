54a7950d8a7eb84b1c0e4b10d59a541d27d9aa76
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-09 18:56:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import config
 
 app = Client(
   "mybot",
-  api_id="API_ID",
-  api_hash="API_HASH",
-  bot_token="BOT_TOKEN"
+  api_id=API_ID,
+  api_hash=API_HASH,
+  bot_token=BOT_TOKEN
 )
 
 app.on_message(filters.command(start))
