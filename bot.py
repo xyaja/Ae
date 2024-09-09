32e58d22b29a236681a6f037ac4cc1d2db910fa2
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-09 18:38:10 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+from pyrogram import Client, filters
 
+app = Client(
+  "mybot"
+  api_id="API_ID"
+  api_hash="API_HASH"
+  bot_token="BOT_TOKEN"
+)
+
+app.on_message(filters.command(start))
+async def start(client, message)
+await message.reply_text(hay)
+
+app.run()
