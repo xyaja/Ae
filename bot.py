a970c5e2a5973419ef9bbfba5569ad5bbc2015fd
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-11 17:22:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,60 +28,21 @@ async def send_photo(client, message):
         await message.reply_text(f"eror send photo: {e}")
       
 @app.on_callback_query(filters.regex("dana"))
-async def callback_query(client, callback_query):
-  await callback_query.message.reply_text("DANA 085175176376")
+async def dana_handler(_, query: CallbackQuery):
+    await query.answer("Anda telah memilih metode pembayaran DANA.")
+    
+    nomor tujuan = 085175176376
+    atas nama = CAS****H
 
-@app.on_message(filters.command('users') & filters.private & filters.user("OWNER"))
-async def get_users(client: app, message):
-    msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
-    users = await full_userbase()
-    await msg.edit(f"{len(users)} users are using this bot")
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "KEMBALI", callback_data="dana"
+                ),
+            ]
+        ]
+    )
 
-@app.on_message(filters.command("broadcast") & filters.private)
-async def send_text(client: app, message):
-    if message.reply_to_message:
-        query = await full_userbase()
-        broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
-        
-        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
-        for chat_id in query:
-            try:
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except UserIsBlocked:
-                await del_user(chat_id)
-                blocked += 1
-            except InputUserDeactivated:
-                await del_user(chat_id)
-                deleted += 1
-            except:
-                unsuccessful += 1
-                pass
-            total += 1
-        
-        status = f"""<b><u>Broadcast Completed</u>
-
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
-        
-        return await pls_wait.edit(status)
-
-    else:
-        msg = await message.reply(REPLY_ERROR)
-        await asyncio.sleep(8)
-        await msg.delete()
-  
 if __name__=="__main__":
    app.run()
