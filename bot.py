9fbc3af03c236e55d374a144afb35b3f1971ef03
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-12 05:07:24 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import asyncio
 from pyrogram import Client, filters
-from pyrogram.raw import functions .types
+from pyrogram.raw import functions, types
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import API_ID, API_HASH, BOT_TOKEN, OWNER
 from database.database import add_user, del_user, full_userbase, present_user 
