8192f1b4bfd2b12e1259e9d94619c0e64893f149
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-12 23:43:45 +0700

    Delete utils directory

--- utils/helpers.py
+++
@@ -1,7 +0,0 @@
-# utils/helpers.py
-
-from pyrogram import Client
-
-async def check_admin(client: Client, chat_id: int) -> bool:
-    chat_member = await client.get_chat_member(chat_id, client.me.id)
-    return chat_member.status in ['administrator', 'creator']
