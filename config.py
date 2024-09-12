696ba2c0960f4f3e0c63afab2e3ce9e4960c64e9
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-12 23:47:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 API_ID = "22412440"
 API_HASH = "211165a095cd129a58bc1d23515c01ef"
 BOT_TOKEN = "7076412312:AAEuWXMtJaNldQq012gXShXBgX3TPq-WUZY"
-
-CHANNELS = {}  # Format: {'keyword': ['-100', '-101']}
-#DB_URI = "mongodb+srv://otwnyoba:Rizqi1687.@on.7tzx4.mongodb.net/?retryWrites=true&w=majority&appName=on"
+MONGO_URI = "mongodb+srv://otwnyoba:Rizqi1687.@on.7tzx4.mongodb.net/?retryWrites=true&w=majority&appName=on"
 #DB_NAME = "on"
 #OWNER = "7526061771"
