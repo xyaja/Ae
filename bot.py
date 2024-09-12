e7a0b812ba65274ea3692f4132272d1f4d3f449c
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-12 23:07:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,24 @@
-import asyncio
+# bot.py
+
 from pyrogram import Client, filters
-from pyrogram.raw import functions, types
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from config import API_ID, API_HASH, BOT_TOKEN
+from handlers.start_handler import start
+from handlers.addch_handler import addch
+from handlers.toch_handler import toch
 
-
-app = Client(
-  "mybot",
-  api_id=API_ID,
-  api_hash=API_HASH,
-  bot_token=BOT_TOKEN,
-)
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
-async def start(client,message):
-    photo = "qris.jpg"
-    caption = "test"
-    button = InlineKeyboardMarkup([
-    [InlineKeyboardButton("DANA", callback_data="dana")]
-    ])
-        await send_photo(photo, caption=caption, raply_markup=keyboard)
+async def start_handler(client, message):
+    await start(client, message)
 
-app.on_callback_query()
-async def handle_callback_query( client, query):
-    if query.data == "dana":
-        await query.answer("085175176376")
-    #elif query.data == "gopay":
-        # await query.answer("085175176376")
+@app.on_message(filters.command("addch"))
+async def addch_handler(client, message):
+    await addch(client, message)
 
-app.run()
+@app.on_message(filters.command("toch"))
+async def toch_handler(client, message):
+    await toch(client, message)
+
+if __name__ == "__main__":
+    app.run()
