da4e2db51e87fffca52d1650d760e703406ff35c
xyaja <163610653+xyaja@users.noreply.github.com>
2024-09-16 22:07:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from config import API_ID, API_HASH, BOT_TOKEN
 
-app = Client("my_bot", api_id="API_ID", api_hash="API_HASH, bot_token="BOT_TOKEN")
+app = Client("my_bot", api_id="API_ID", api_hash="API_HASH", bot_token="BOT_TOKEN")
 
 # Daftar untuk menyimpan ID channel
 channel_ids = []
